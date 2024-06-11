933ca553db774d9898dba3ca2ae376a0147f6d30
Xentios <xenq@hotmail.com>
2024-06-11 21:26:14 +0300

    Update Interactables.cs
    Now object reacts differently to each instrument. Code structure is bad.

--- Assets/Interactables.cs
+++ Assets/Interactables.cs
@@ -5,11 +5,15 @@ public class Interactables : MonoBehaviour
 
     [SerializeField]
     private float interractionDistance = 3f;
-    private GameObject player;
 
+
+
+    private GameObject player;
+    private PlayerInstrument playerInstrument;
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        playerInstrument = player.GetComponent<PlayerInstrument>();
     }
 
     public void Interact()
@@ -17,6 +21,21 @@ public class Interactables : MonoBehaviour
         if (Vector2.Distance(player.transform.position, transform.position) > interractionDistance) return;
 
         Debug.Log("Player Interracted With Me");
-        transform.localScale *= 2;
+        var currentInstrumentType = playerInstrument.selectedInstrument.instrumentType;
+        switch (currentInstrumentType)
+        {
+            case PlayerInstrument.InstrumentType.Wind:
+            transform.transform.position += Vector3.up / 2;
+            break;
+            case PlayerInstrument.InstrumentType.Percussion:
+            transform.localScale /= 2;
+            break;
+            case PlayerInstrument.InstrumentType.String:
+            transform.localScale *= 2;
+            break;
+            default:
+            break;
+        }
+
     }
 }
