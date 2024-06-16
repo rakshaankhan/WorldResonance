438108c117b89077cd0409bb2c91e2d38ef4dcc3
Byungkyu Lee <browningno1@gmail.com>
2024-06-16 14:33:57 -0700

    Random Audio Player Script

---
+++ Assets/Scripts/Audio/RandomOneShot.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class RandomOneShot : MonoBehaviour
+{
+
+    public AudioClip[] audioClips;
+    int Audio;
+    AudioSource audioSource;
+
+    // Use this for initialization
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void triggerPull()
+	{
+		Audio = Random.Range(0, audioClips.Length);
+		audioSource.PlayOneShot(audioClips[Audio]);
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetButtonUp("Jump"))
+        //{
+
+            //Audio = Random.Range(0, audioClips.Length);
+            //audioSource.PlayOneShot(audioClips[Audio]);
+            //Debug.Log("Jump input detected! " + Audio);
+        //}
+    }
+}
