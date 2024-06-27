a7a6104aa90878b805875b2757e89780f499a64d
Xentios <xenq@hotmail.com>
2024-06-27 21:13:07 +0300

    Update MoveBridge.cs
    Added scale option

--- Assets/Scripts/Other/MoveBridge.cs
+++ Assets/Scripts/Other/MoveBridge.cs
@@ -7,11 +7,17 @@ public class MoveBridge : MonoBehaviour
     Vector3 direction;
 
     [SerializeField]
+    Vector3 scale = Vector3.one;
+
+    [SerializeField]
     float timer;
 
+    [SerializeField]
+    float scaletimer = 1f;
 
     public void MoveTransform()
     {
         transform.DOBlendableMoveBy(direction, timer);
+        transform.DOScale(scale, scaletimer);
     }
 }
